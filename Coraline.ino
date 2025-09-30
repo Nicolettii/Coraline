c6ce5cbc91adb883c6e5ca6bd40fce7d38dd635b
nicolettii <thiagognicoletti@gmail.com>
2025-09-30 13:36:18 -0300

    changed to display the info on the lcd

--- Coraline.ino
+++ Coraline.ino
@@ -1,12 +1,21 @@
 #include <DHT.h>
+#include <LiquidCrystal_I2C.h>
 
 #define dhtpin 4
 #define dhtype DHT11
 DHT dht(dhtpin, dhtype);
 
+#define lcdcol 16
+#define lcdrow 2
+#define addr 0x27
+LiquidCrystal_I2C lcd(addr, lcdcol, lcdrow);
+
 void setup() {
 Serial.begin(115200);
 dht.begin();
+lcd.init();
+lcd.clear();
+lcd.backlight();
 }
 
 void loop() {
@@ -17,12 +26,14 @@ void read() {
 delay(2000);
 float temp = dht.readTemperature();
 float humi = dht.readHumidity();
-Serial.print("T:");
-Serial.print(temp);
-Serial.print((char)223);
-Serial.println("C");
+lcd.setCursor(0, 0);
+lcd.print("T:");
+lcd.print(temp);
+lcd.print((char)223);
+lcd.print("C");
 
-Serial.print("U:");
-Serial.print(humi);
-Serial.println("%");
+lcd.setCursor(0, 1);
+lcd.print("U:");
+lcd.print(humi);
+lcd.print("%");
 }
